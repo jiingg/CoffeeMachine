04e1b88372f42946fc0895b68a3e820f42d70d9b
jiingg <yanji.bb@gmail.com>
2020-04-01 23:05:08 -0400

    changes

--- src/Main.java
+++ src/Main.java
@@ -41,78 +41,102 @@ public class Main {
         System.out.println("Write action (buy, fill, take, remaining, exit):");
         String action = sc.nextLine();
 
-        while (!action.equals("quit")){
-            while (action.equals("buy")){
-                System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, " +
-                        "back - to main menu:");
-                String buyChoice = sc.nextLine();
-                if (buyChoice.equals("1")) {
-                    if (esprWater <= water && esprBeans <= beans && disposableCups > 0) {
-                        water -= esprWater;
-                        beans -= esprBeans;
-                        money += esprMoney;
-                        disposableCups -= 1;
+        while (!action.equals("exit")) {
 
-                        System.out.println("I have enough resources, making you a coffee!");
-                        break;
-                    } else if (esprWater > water && esprBeans > beans && disposableCups < 0) {
-                        System.out.println("Sorry, not enough resources!");
-                    } else if (esprWater > water) {
-                        System.out.println("Sorry, not enough water!");
-                    } else if (esprBeans > beans) {
-                        System.out.println("Sorry, not enough beans!");
-                    } else if (disposableCups < 0) {
-                        System.out.println("Sorry, not enough cups!");
-                    }
-                    break;
-                } else if (buyChoice.equals("2")) {
-                    if (latteWater <= water && latteMilk <= milk && latteBeans <= beans && disposableCups > 0) {
-                        water -= latteWater;
-                        milk -= latteMilk;
-                        beans -= latteBeans;
-                        money += latteMoney;
-                        disposableCups -= 1;
+            if (!action.equals("quit")) {
+                while (action.equals("buy")) {
+                    System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, " +
+                            "back - to main menu:");
+                    String buyChoice = sc.nextLine();
+                    if (buyChoice.equals("1")) {
+                        if (esprWater <= water && esprBeans <= beans && disposableCups > 0) {
+                            water -= esprWater;
+                            beans -= esprBeans;
+                            money += esprMoney;
+                            disposableCups -= 1;
 
-                        System.out.println("I have enough resources, making you a coffee!");
+                            System.out.println("I have enough resources, making you a coffee!");
+                            break;
+                        } else if (esprWater > water && esprBeans > beans && disposableCups < 0) {
+                            System.out.println("Sorry, not enough resources!");
+                        } else if (esprWater > water) {
+                            System.out.println("Sorry, not enough water!");
+                        } else if (esprBeans > beans) {
+                            System.out.println("Sorry, not enough beans!");
+                        } else if (disposableCups < 0) {
+                            System.out.println("Sorry, not enough cups!");
+                        }
                         break;
-                    } else if (latteWater > water && latteMilk > milk && latteBeans > beans && disposableCups < 0) {
-                        System.out.println("Sorry, not enough resources!");
-                    } else if (latteWater > water) {
-                        System.out.println("Sorry, not enough water!");
-                    } else if (latteMilk > milk) {
-                        System.out.println("Sorry, not enough milk!");
-                    } else if (latteBeans > beans) {
-                        System.out.println("Sorry, not enough beans!");
-                    } else if (disposableCups < 0) {
-                        System.out.println("Sorry, not enough cups!");
-                    }
-                    break;
-                } else if (buyChoice.equals("3")) {
-                    if (cappWater <= water && cappMilk <= milk && cappBeans <= beans && disposableCups > 0) {
-                        water -= cappWater;
-                        milk -= cappMilk;
-                        beans -= cappBeans;
-                        money += cappMoney;
-                        disposableCups -= 1;
+                    } else if (buyChoice.equals("2")) {
+                        if (latteWater <= water && latteMilk <= milk && latteBeans <= beans && disposableCups > 0) {
+                            water -= latteWater;
+                            milk -= latteMilk;
+                            beans -= latteBeans;
+                            money += latteMoney;
+                            disposableCups -= 1;
 
-                        System.out.println("I have enough resources, making you a coffee!");
+                            System.out.println("I have enough resources, making you a coffee!");
+                            break;
+                        } else if (latteWater > water && latteMilk > milk && latteBeans > beans && disposableCups < 0) {
+                            System.out.println("Sorry, not enough resources!");
+                        } else if (latteWater > water) {
+                            System.out.println("Sorry, not enough water!");
+                        } else if (latteMilk > milk) {
+                            System.out.println("Sorry, not enough milk!");
+                        } else if (latteBeans > beans) {
+                            System.out.println("Sorry, not enough beans!");
+                        } else if (disposableCups < 0) {
+                            System.out.println("Sorry, not enough cups!");
+                        }
                         break;
-                    } else if (cappWater > water && cappMilk > milk && cappBeans > beans && disposableCups < 0) {
-                        System.out.println("Sorry, not enough resources!");
-                    } else if (cappWater > water) {
-                        System.out.println("Sorry, not enough water!");
-                    } else if (cappMilk > milk) {
-                        System.out.println("Sorry, not enough milk!");
-                    } else if (cappBeans > beans) {
-                        System.out.println("Sorry, not enough beans!");
-                    } else if (disposableCups < 0) {
-                        System.out.println("Sorry, not enough cups!");
+                    } else if (buyChoice.equals("3")) {
+                        if (cappWater <= water && cappMilk <= milk && cappBeans <= beans && disposableCups > 0) {
+                            water -= cappWater;
+                            milk -= cappMilk;
+                            beans -= cappBeans;
+                            money += cappMoney;
+                            disposableCups -= 1;
+
+                            System.out.println("I have enough resources, making you a coffee!");
+                            break;
+                        } else if (cappWater > water && cappMilk > milk && cappBeans > beans && disposableCups < 0) {
+                            System.out.println("Sorry, not enough resources!");
+                        } else if (cappWater > water) {
+                            System.out.println("Sorry, not enough water!");
+                        } else if (cappMilk > milk) {
+                            System.out.println("Sorry, not enough milk!");
+                        } else if (cappBeans > beans) {
+                            System.out.println("Sorry, not enough beans!");
+                        } else if (disposableCups < 0) {
+                            System.out.println("Sorry, not enough cups!");
+                        }
+                        break;
+                    } else if (buyChoice.equals("back")) {
+                        System.out.println("Write action (buy, fill, take, remaining, exit):");
+                        action = sc.nextLine();
+                        break;
                     }
-                    break;
-                } else if (buyChoice.equals("back")) {
-                    break;
                 }
 
+                while (action.equals("fill")){
+                    System.out.println("Write how many ml of water do you want to add:");
+                    int addWater = sc.nextInt();
+                    System.out.println("Write how many ml of milk do you want to add:");
+                    int addMilk = sc.nextInt();
+                    System.out.println("Write how many grams of coffee beans do you want to add:");
+                    int addBeans = sc.nextInt();
+                    System.out.println("Write how many disposable cups do you want to add:");
+                    int addDispCups = sc.nextInt();
+
+                    water += addWater;
+                    milk += addMilk;
+                    beans += addBeans;
+                    disposableCups += addDispCups;
+
+                    System.out.println("Write action (buy, fill, take, remaining, exit):");
+                    action = sc.nextLine();
+                    break;
+                }
             }
         }
 
